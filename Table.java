7bf1f74ad98e4a54de2b9d1d4ab7ad80bd40b78b
Mr Kiedes <akiedes@vtsd.com>
2019-12-09 08:36:34 -0500

    Setter Updates

--- Table.java
+++ Table.java
@@ -1,22 +1,23 @@
 public class Table{
-  double width, height;
-
+  // Attributes
+  private double width;
+  private double height;
+  // Constructors
   public Table(){
     width = 60;
     height = 36;
   }
-
   public Table(double width){
     this.width = width;
     height = 36;
   }
-
   public Table(double width, double height){
     this.width = width;
     this.height = height;
   }
 
   public double getWidth(){ return width; }
-
   public double getHeight(){ return height; }
+  public void setWidth(double width){ this.width = width; }
+  public void setHeight(double height){ this.height = height; }
 }
